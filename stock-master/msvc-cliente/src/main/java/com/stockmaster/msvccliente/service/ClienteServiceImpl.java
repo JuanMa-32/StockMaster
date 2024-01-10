075dedcd7995de4e80b4254440f7905de527f274
damian <adamodamian6@gmail.com>
2024-01-10 20:43:01 -0300

    agregando transactional

--- stock-master/msvc-cliente/src/main/java/com/stockmaster/msvccliente/service/ClienteServiceImpl.java
+++ stock-master/msvc-cliente/src/main/java/com/stockmaster/msvccliente/service/ClienteServiceImpl.java
@@ -4,6 +4,7 @@ import com.stockmaster.msvccliente.entity.Cliente;
 import com.stockmaster.msvccliente.repository.ClienteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
@@ -12,21 +13,25 @@ public class ClienteServiceImpl implements ClienteService{
     @Autowired
     private ClienteRepository clienteRepository;
     @Override
+    @Transactional(readOnly = true)
     public List<Cliente> findAll() {
         return clienteRepository.findAll();
     }
 
     @Override
+    @Transactional(readOnly = true)
     public Optional<Cliente> findById(Long id) {
         return clienteRepository.findById(id);
     }
 
     @Override
+    @Transactional
     public Cliente save(Cliente cliente) {
         return clienteRepository.save(cliente);
     }
 
     @Override
+    @Transactional
     public void deleteById(Long id) {
         clienteRepository.deleteById(id);
 
