b4246fcc26ce4a3132220a66b79aecc8b6fd0a57
damian <adamodamian6@gmail.com>
2024-01-29 19:38:36 -0300

    empezando msvc-negocio

---
+++ stock-master/msvc-negocio/src/main/java/com/negocio/msvcnegocio/MsvcNegocioApplication.java
@@ -0,0 +1,13 @@
+package com.negocio.msvcnegocio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsvcNegocioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsvcNegocioApplication.class, args);
+	}
+
+}
