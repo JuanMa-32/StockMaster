69696dda01654a8aca5230b71062254978b1f434
damian <adamodamian6@gmail.com>
2024-01-31 20:15:25 -0300

    agregando autowired

--- stock-master/msvc-producto/src/main/java/com/stockmaster/msvcproducto/service/CategoriaServiceImpl.java
+++ stock-master/msvc-producto/src/main/java/com/stockmaster/msvcproducto/service/CategoriaServiceImpl.java
@@ -2,12 +2,14 @@ package com.stockmaster.msvcproducto.service;
 
 import com.stockmaster.msvcproducto.entity.Categoria;
 import com.stockmaster.msvcproducto.repository.CategoriaRepositorio;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 @Service
 public class CategoriaServiceImpl implements CategoriaService{
+    @Autowired
     private CategoriaRepositorio categoriaRepositorio;
     @Override
     public List<Categoria> findAll() {
