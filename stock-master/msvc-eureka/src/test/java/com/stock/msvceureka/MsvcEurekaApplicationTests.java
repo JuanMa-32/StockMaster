aaf477a1ba07c853c69e37c2e0f50a8d7ac3801e
damian <adamodamian6@gmail.com>
2024-02-08 20:34:55 -0300

    agregando eureka y gateway

---
+++ stock-master/msvc-eureka/src/test/java/com/stock/msvceureka/MsvcEurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stock.msvceureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsvcEurekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
